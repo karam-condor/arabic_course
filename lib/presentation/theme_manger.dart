75da0c4d8ed91c0b37231303e66a685526958fca
Karam Yakuob Agha <karam.agha@condorbrasil.br>
2022-10-03 15:18:38 -0300

    1st version

---
+++ lib/presentation/theme_manger.dart
@@ -0,0 +1,110 @@
+import 'package:arabic_app/presentation/font_manger.dart';
+import 'package:arabic_app/presentation/styles_manger.dart';
+import 'package:arabic_app/presentation/values_manger.dart';
+
+import 'color_manger.dart';
+import 'package:flutter/material.dart';
+
+ThemeData getApplicationTheme() {
+  return ThemeData(
+    // main colors
+    primaryColor: ColorManager.primary,
+    primaryColorLight: ColorManager.lightPrimary,
+    primaryColorDark: ColorManager.darkPrimary,
+    disabledColor: ColorManager.grey1,
+    // cardview theme
+    cardTheme: CardTheme(
+        color: ColorManager.white,
+        shadowColor: ColorManager.grey,
+        elevation: AppSize.s4),
+    // app bar theme
+
+    appBarTheme: AppBarTheme(
+        centerTitle: true,
+        color: ColorManager.primary,
+        elevation: AppSize.s4,
+        shadowColor: ColorManager.lightPrimary,
+        titleTextStyle:
+            getRegularStyle(fontSize: FontSize.s16, color: ColorManager.white)),
+
+    // button theme
+
+    // button theme
+    buttonTheme: ButtonThemeData(
+        shape: const StadiumBorder(),
+        disabledColor: ColorManager.grey1,
+        buttonColor: ColorManager.primary,
+        splashColor: ColorManager.lightPrimary),
+
+    // text theme
+    // elevated button them
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        textStyle:
+            getRegularStyle(color: ColorManager.white, fontSize: FontSize.s17),
+        primary: ColorManager.primary,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(AppSize.s12),
+        ),
+      ),
+    ),
+
+    // text theme
+
+    textTheme: TextTheme(
+      displayLarge: getLightStyle(color: Colors.white, fontSize: FontSize.s22),
+      headline1: getSemiBoldStyle(
+          color: ColorManager.darkGrey, fontSize: FontSize.s16),
+      subtitle1:
+          getMediumStyle(color: ColorManager.lightGrey, fontSize: FontSize.s14),
+      caption: getRegularStyle(color: ColorManager.grey1),
+      bodyText1: getRegularStyle(color: ColorManager.grey),
+    ),
+
+    // input decoration theme (text form field)
+    inputDecorationTheme: InputDecorationTheme(
+      //content padding
+      contentPadding: const EdgeInsets.all(AppPadding.p12),
+      //hint style
+      hintStyle:
+          getRegularStyle(color: ColorManager.grey, fontSize: FontSize.s14),
+      //Label
+      labelStyle:
+          getMediumStyle(color: ColorManager.grey, fontSize: FontSize.s14),
+      //Error Style
+      errorStyle: getRegularStyle(color: ColorManager.error),
+      //Enabled Border
+      enabledBorder: OutlineInputBorder(
+        borderSide:
+            BorderSide(color: ColorManager.primary, width: AppSize.s1_5),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(AppSize.s18),
+        ),
+      ),
+      //Focused Border
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: ColorManager.grey, width: AppSize.s1_5),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(AppSize.s18),
+        ),
+      ),
+      //Error border style
+      errorBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: ColorManager.error,
+          width: AppSize.s1_5,
+        ),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(AppSize.s18),
+        ),
+      ),
+      focusedErrorBorder: OutlineInputBorder(
+        borderSide:
+            BorderSide(color: ColorManager.primary, width: AppSize.s1_5),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(AppSize.s18),
+        ),
+      ),
+    ),
+  );
+}
